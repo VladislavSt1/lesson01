0422b164101c3e2c37102b3ed7bbb3d4485ff33f
Vladislav <stupneff2014@yandex,ru>
2020-08-31 14:15:55 +0300

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,6 @@ const mix = require('laravel-mix');
 
 mix.sass('resources/sass/main.scss', 'public/css');
 mix.js('resources/js/main.js', 'public/js');
+mix.setPublicPath(
+    'public'
+ ).setResourceRoot('../');
